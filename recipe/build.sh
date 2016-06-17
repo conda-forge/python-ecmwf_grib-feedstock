29202def61adc1a0f880e20a6a5e74873c72d0c0
Kai Mühlbauer <kmuehlbauer@users.noreply.github.com>
2016-06-17 20:05:19 +0200

    WIP: use cmake build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# This is just ecmwf_grib/recipe/build.sh with the `--enable-python` turned on.
-
 if [[ $(uname) == Darwin ]]; then
   export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
 elif [[ $(uname) == Linux ]]; then
@@ -13,18 +11,20 @@ export PYTHON_LDFLAGS="$PREFIX/lib"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
 
-./configure --prefix=$PREFIX \
-            --with-jasper=$PREFIX \
-            --with-netcdf=$PREFIX \
-            --with-png-support \
-            --disable-fortran \
-            --enable-python
+src_dir="$(pwd)"
+mkdir ../build
+cd ../build
+cmake $src_dir \
+         -DCMAKE_INSTALL_PREFIX=$PREFIX \
+         -DENABLE_JPG=1 \
+         -DENABLE_NETCDF=1 \
+         -DENABLE_PNG=1 \
+         -DENABLE_PYTHON=1 \
+         -DENABLE_FORTRAN=0
 
 make
 make install
 
-# For some reason the installer places the Python files in a sub-directory of site-packages called "grib_api". (NB. The sub-directory is not a package.)
-# The install instructions in python/README include the suggestion:
-# Add this folder to your PYTHONPATH and you are ready to go. Instead of that, we just rename the directory and make it a package.
-mv $SP_DIR/grib_api $SP_DIR/gribapi
-mv $SP_DIR/gribapi/gribapi.py $SP_DIR/gribapi/__init__.py
+if [[ $(uname) == Darwin ]]; then
+  ln -s $SP_DIR/gribapi/_gribapi_swig.dylib $SP_DIR/gribapi/_gribapi_swig.so
+fi
