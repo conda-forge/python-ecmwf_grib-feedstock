3bfad3d0daac499a4d43b6c9607f7ac1e2e1b5b9
ocefpaf <ocefpaf@gmail.com>
2016-06-20 18:08:56 -0300

    Find python

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,17 +11,14 @@ export PYTHON_LDFLAGS="$PREFIX/lib"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
 
-src_dir="$(pwd)"
-mkdir ../build
-cd ../build
-cmake $src_dir \
-         -DCMAKE_INSTALL_PREFIX=$PREFIX \
-         -DENABLE_JPG=1 \
-         -DENABLE_NETCDF=1 \
-         -DENABLE_PNG=1 \
-         -DENABLE_PYTHON=1 \
-         -DENABLE_FORTRAN=0
-
+mkdir build_ecmwf && cd build_ecmwf
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D ENABLE_JPG=1 \
+      -D ENABLE_NETCDF=1 \
+      -D ENABLE_PNG=1 \
+      -D ENABLE_FORTRAN=0 \
+      -D ENABLE_PYTHON=1 \
+      $SRC_DIR
 make
 make install
 
